f35b8d4b41ca8fe1edc515fa915d67ec2401f954
Mahbubullah <mahbubullahpathan@gmail.com>
2023-08-15 00:28:28 +0600

    font

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,13 @@
 /** @type {import('tailwindcss').Config} */
+
 module.exports = {
     content: ["./src/**/*.{html,js}"],
     theme: {
-      extend: {},
+      extend: {
+        fontFamily: {
+          'poppins': ['Poppins', 'sans-serif']
+        }
+      },
     },
     plugins: [],
   }
